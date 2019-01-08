54bd759c525fb842fd333910c3d478920e3dd466
Eli Cleveland <elicleveland12@gmail.com>
2019-01-08 20:24:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 class Dog 
   
-  def initialize(name, breed)
-    @name = name
-    @breed = breed 
+  def initialization
+    if initialize(name, breed)
+      @name = name 
+      @breed = breed
+    elsif initialize(name)
+      @name = name 
+      
+    
   end 
   
   def name 
